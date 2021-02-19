cc9c1f8cf00533b7cbe142925a4cc9b6c513e7ae
Nairi Narinyan <nairi.narinyan@gmail.com>
2021-02-19 12:07:01 +0400

    rm compiled files

--- source/src/shared/history.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.history = void 0;
-var history_1 = require("history");
-exports.history = history_1.createBrowserHistory();
